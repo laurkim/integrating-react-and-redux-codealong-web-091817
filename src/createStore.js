52bbee1e79d3993f9a6a612e87165aa1ba2239ee
laurkim <laurakeem@gmail.com>
2017-12-11 12:02:10 -0500

    Done.

--- src/createStore.js
+++ src/createStore.js
@@ -1,1 +1,39 @@
+import { render } from './index.js';
 
+export default function createStore(reducer) {
+  let state;
+
+  const dispatch = action => {
+    state = reducer(state, action);
+    console.log(`the state is ${state.count}`);
+    console.log(`the action is ${action.type}`);
+    render();
+  };
+
+  const getState = () => {
+    return state;
+  };
+
+  return { dispatch, getState };
+};
+
+
+// export default function createStore(reducer) {
+//   let state;
+//
+//   function dispatch(action) {
+//     state = reducer(state, action);
+//     console.log(`the state is ${state.count}`);
+//     console.log(`the action is ${action.type}`);
+//     render();
+//   };
+//
+//   function getState(){
+//     return state;
+//   };
+//
+//   return {
+//     dispatch,
+//     getState
+//   };
+// };
---
+++ src/reducers/changeCount.js
@@ -0,0 +1,33 @@
+// export default function changeCount(state = { count: 0 }, action) {
+//   switch (action.type) {
+//     case 'INCREASE_COUNT':
+//       return { count: state.count + 1 };
+//     default:
+//       return state;
+//   };
+// };
+
+
+// function changeCount(state = {
+//   count: 0,
+// }, action) {
+//   switch (action.type) {
+//     case 'INCREASE_COUNT':
+//       return { count: state.count + 1 };
+//     default:
+//       return state;
+//   };
+// };
+
+
+export default function changeCount(state = {
+  count: 0
+}, action) {
+  switch (action.type) {
+    case 'INCREASE_COUNT':
+      return { count: state.count + 1 };
+
+    default:
+      return state;
+  };
+};
